7b0fb097495805479fa4112723d280ca10264338
woofan <495464616@qq.com>
2016-06-23 23:27:14 +0800

    6.23 start

---
+++ daily.js
@@ -0,0 +1,9 @@
+function headertime()
+{
+	var d= new Date();
+	year=d.getFullYear();
+	month=d.getMonth();
+	date=d.getDate();
+	document.getElementById("time").innerHTML="fadf";
+	/*a.innerText="今天是"+year+"年"+month+"月"+date+"日";*/
+}
