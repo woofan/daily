2fcb7e2994ef881b178d5802a530cc9e535d4599
woofan <495464616@qq.com>
2016-09-06 23:27:57 +0800

    9.6

--- daily.js
+++ daily.js
@@ -1,9 +1,7 @@
-function headertime()
-{
-	var d= new Date();
-	year=d.getFullYear();
-	month=d.getMonth();
-	date=d.getDate();
-	document.getElementById("time").innerHTML="fadf";
-	/*a.innerText="今天是"+year+"年"+month+"月"+date+"日";*/
+function changeHref(){
+	
+}
+function getHref(obj){
+	var href = obj.getAttribute("href");
+	alert(href);
 }
