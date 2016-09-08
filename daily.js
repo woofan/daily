56962d8b171ada829556ec19a252e578d206e7ed
woofan <495464616@qq.com>
2016-09-08 09:34:19 +0800

    9.8

--- daily.js
+++ daily.js
@@ -1,8 +1,18 @@
+window.onload = main;
 function changeHref(href){
-	img = document.getElementsByTagName("img");
+	img = document.getElementsByTagName("img")[0];
 	img.setAttribute("src",href);
 }
-function getHref(obj){
-	var href = obj.getAttribute("href");
-	changeHref(href);
+function main(){
+	a_list = document.getElementsByTagName("a");
+	for(var i=0;i<a_list.length;i++){
+		a_list[i].onclick = function(){
+			changeHref(this.getAttribute("href"));
+			showpic(this.getAttribute("href"));
+			return false;
+		}
+	} 
+}
+function showpic(href){
+	window.open(href,"wf","width=400,height=300");
 }
