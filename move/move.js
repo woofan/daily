5d393498fb0a4befd74dd5436ed22177601ff90c
woofan <495464616@qq.com>
2016-09-16 19:47:12 +0800

    9.16

--- move/move.js
+++ move/move.js
@@ -10,49 +10,51 @@ function addLoadEvent(func){
 		}
 	}	
 }
-function positionMessage(){
-	var elem = document.getElementById("rec");
-	elem.style.position = "absolute";
-	elem.style.left = "50px";
-	elem.style.top = "100px";
-	//movement = setTimeout("moveMessage()",5000);//函数内部不用var定义的变量是全局变量
+function positionMessage(element,position,left,top){
+	var elem = document.getElementById(element);
+	elem.style.position = position;
+	elem.style.left = left;
+	elem.style.top = top;
 
 }
-function moveMessage(){
-	var elem = document.getElementById("rec");
-	var left = parseInt(elem.style.left);
-	var top = parseInt(elem.style.top); 
-	if(left < 600) {
-		left+=10;
-	}
-	if(left == 600){
-		top+=10;
-	}
-	if(top == 500){
-		backMessage();
+function moveMessage(element,left_to,pertime){
+	var elem = document.getElementById(element);
+	var left_now = parseInt(elem.style.left);
+	var top_now = parseInt(elem.style.top);
+	if(elem.movement){
+		clearTimeout(elem.movement);
+	} 
+	if(left_now == left_to) {
 		return true;
 	}
-	elem.style.left = left + "px";
-	elem.style.top = top + "px";
-	movement = setTimeout("moveMessage()",50);
+	if(left_now > left_to){
+		left_now-=Math.ceil((left_now-left_to)/30);
+	}
+	if(left_now < left_to){
+		left_now+=Math.ceil((left_to-left_now)/30);
+	}
+	elem.style.left = left_now + "px";
+	var repeat = "moveMessage(\""+element+"\","+left_to+","+pertime+")";
+	elem.movement = setTimeout(repeat,pertime);//函数内部不用var定义的变量是全局变量
 }
-function backMessage(){
-	var elem = document.getElementById("rec");
-	var left = parseInt(elem.style.left);
-	var top = parseInt(elem.style.top);
-	if(left > 50){
-		left -=10;
+function showPicture(){
+	var pic = document.getElementById("pic");
+	var list = document.getElementsByTagName("a");
+	// for(var i=0;i<list.length;i++){
+	// 	list[i].onmouseover  = function(){
+	// 		moveMessage("pic",-200*i,1);
+	// 	}
+	// }
+	list[0].onmouseover  = function(){
+		moveMessage("pic",0,1);
 	}
-	if(left == 50){
-		top -=10;
+	list[1].onmouseover  = function(){
+		moveMessage("pic",-200,1);
 	}
-	if(top == 100){
-		moveMessage();
-		return true;
+	list[2].onmouseover  = function(){
+		moveMessage("pic",-400,1);
 	}
-	elem.style.left = left + "px";
-	elem.style.top = top + "px";
-	movement = setTimeout("backMessage()",50);
 }
-addLoadEvent(positionMessage);
-addLoadEvent(moveMessage);
+addLoadEvent(positionMessage("show","relative","0px","200px"));
+addLoadEvent(positionMessage("pic","absolute","0px","0px"));
+addLoadEvent(showPicture());
