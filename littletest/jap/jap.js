acbb10ac97ed46accfeb440cf538b24bd650ba73
woofan <495464616@qq.com>
2017-03-25 14:36:49 +0800

    3/25

--- littletest/jap/jap.js
+++ littletest/jap/jap.js
@@ -1,4 +1,5 @@
 getElement("input").textContent = "";
+getElement("time").textContent = "0秒";
 keyBoard();
 addEvent("key");
 getElement("delete").addEventListener("click", deleteText, false);
@@ -7,14 +8,29 @@ getElement("change").addEventListener("click", randomWord, false);
 getElement("answer").addEventListener("click", showAnswer, false);
 let [word, list] = getWord();
 randomWord();
+let time = 0;
+setInterval(showTime, 1000);
 
-function getElement(ele) {
+function getElement(ele) { //返回指定id的DOM对象
     if (ele) {
         return document.getElementById(ele);
     } else return -1;
 }
 
-function keyBoard() {
+function showTime() {
+    time++;
+    let times = "";
+    if (time < 60) {
+        times = time + "秒";
+    } else {
+        let min = Math.floor(time / 60);
+        let sec = time % 60;
+        times = min + " 分 " + sec + " 秒";
+    }
+    getElement("time").textContent = times;
+}
+
+function keyBoard() { //生成键盘
     let keyboard = getElement("keyboard");
     let list = ["a", "i", "u", "e", "o", "k", "s", "t", "n", "h", "m", "y", "r", "w", "n", "u", "c", "f"];
     for (let n in list) {
@@ -24,7 +40,7 @@ function keyBoard() {
     }
 }
 
-function createDiv(location, content) {
+function createDiv(location, content) { //在指定父元素内创建指定内容的键盘的一个键
     let text = (typeof(content) === "string") ? content : ("" + content);
     if (typeof(location) === "object") {
         let div = document.createElement("div");
@@ -35,12 +51,12 @@ function createDiv(location, content) {
     } else return -1;
 }
 
-function clickDiv() {
+function clickDiv() { //点击键盘的一个键后将键盘上的字母打印到id为input的dom元素中
     let text = this.lastChild.textContent;
     getElement("input").textContent += text;
 }
 
-function addEvent(classname) {
+function addEvent(classname) { //为键盘的每个键添加点击事件
     let div_list = document.querySelectorAll("." + classname);
     for (let i in div_list) {
         if (div_list.hasOwnProperty(i)) {
@@ -49,7 +65,7 @@ function addEvent(classname) {
     }
 }
 
-function deleteText() {
+function deleteText() { //撤销按钮的点击事件=>删除id为input的dom元素的内容中的最后一个字符
     let text = getElement("input").textContent;
     if (text.length > 0) {
         let newtext = text.substring(0, text.length - 1);
@@ -57,7 +73,7 @@ function deleteText() {
     }
 }
 
-function confirm() {
+function confirm() { //确认按钮的点击事件=>答对就换一个,打错继续
     let answer = getElement("input").textContent;
     if (answer === "") {
         alert("输入为空!");
@@ -76,14 +92,19 @@ function confirm() {
     rightPercentage();
 }
 
-function randomWord() {
-    let n = Math.floor(Math.random() * list.length);
-    let question = list[n];
-    getElement("jap").textContent = "";
-    getElement("jap").textContent += question;
+function randomWord() { //换一个按钮点击事件=>随机换一个日语字符
+    if (list.length > 0) {
+        let n = Math.floor(Math.random() * list.length);
+        let question = list[n];
+        getElement("jap").textContent = "";
+        getElement("jap").textContent += question;
+    } else {
+        alert("全部答完了!");
+        getElement("jap").textContent = "";
+    }
 }
 
-function answerRight(key) {
+function answerRight(key) { //回答正确的情况.该字符对应的正确标记记true,list里对应的数组元素删去,背景改绿色
     word.get(key).right = true;
     word.get(key).times += 1;
     getElement("input").textContent = "";
@@ -94,13 +115,13 @@ function answerRight(key) {
     randomWord();
 }
 
-function answerFalse(key) {
+function answerFalse(key) { //回答错误的情况.背景改红色
     word.get(key).times += 1;
     getElement("input").textContent = "";
     getElement("input").style.backgroundColor = "red";
 }
 
-function showAnswer() {
+function showAnswer() { //显示答案按钮点击事件=>在input里直接显示正确的答案
     let question = getElement("jap").textContent;
     for (let i of word.keys()) {
         if (i === question) {
@@ -110,7 +131,7 @@ function showAnswer() {
     }
 }
 
-function rightPercentage() {
+function rightPercentage() { //统计已正确回答的数量,计算正确率,并在right_percentage里显示
     let times = 0,
         right_times = 0;
     for (let i of word.values()) {
@@ -124,7 +145,7 @@ function rightPercentage() {
     getElement("right_percentage").textContent = "已答对: " + right_word + "! 正确率: " + right_percentage;
 }
 
-function getWord() {
+function getWord() { //将之前对象方式存储的数据用map存储,用数组记录还未答对的日语字符,方便随机抽取.
     let list = [];
     let word = new Map();
     let old_word = {
